a07f11b8a046f92b184ba9ca55adf0ec501a19b7
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 04:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'P
 
 class Route{
   constructor(beginningLocation, endingLocation){
-    this.begX=beginningLocation.horizontal;
+    this.begX=eastWest.index(beginningLocation.horizontal);
     this.begY=beginningLocation.vertical;
     this.endX=endingLocation.horizontal;
     this.endy=endingLocation.vertical;
